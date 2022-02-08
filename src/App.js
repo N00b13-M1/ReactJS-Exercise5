42e4645a204bb73fb68b7069a2e8cd032a9bd44c
N00b13-M1 <michael.cornelis@gmail.com>
2022-02-08 13:40:16 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-// Exercice 5
-
-// Créez un dossier ReactJS qui s'appelle "exercice5".
-// Supprimez tous les dossiers inutiles.
-// Dans le fichier App.js, supprimez tous et créer une class App avec les extends et imports nécessaires.
-// Créez un nouveau fichiers appelé Header.js
-// Dans ce fichier header, vous allez créer une barre de navigation en JSX et l'importer dans votre fichier App.js
-
-
-import React, {Component} from 'react';
-import Header from './Header';
+import React, { Component } from 'react';
+import Header from './components/Header'
 
 class App extends Component {
-  render(){
+  render() {
     return (
-      <Header/>
+      <div>
+        <Header/>
+      </div>
     );
   }
 }
